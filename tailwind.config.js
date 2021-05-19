7b5bb5d08117e36310ed1c53f837319358aa5516
DarkFalcon <rohan.healy@gmail.com>
2021-05-19 16:47:32 +0100

    fixed maybe pls dont break

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {
+          
+      },
+    },
+    variants: {
+      extend: {
+        grayscale: ['hover', 'focus'],
+      },
+    },
+    plugins: [],
+
+  }
